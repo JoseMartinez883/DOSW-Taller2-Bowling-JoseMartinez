a2f41500d4dcd73ee8322930972e5e061aa615f8
Jose Martinez <115952610+JoseMartinez883@users.noreply.github.com>
2026-09-17 15:28:48 -0500

    feat: GREEN - implements isComplete for module C
    Signed-off-by: Jose Martinez <115952610+JoseMartinez883@users.noreply.github.com>

--- bowlin-tdd/src/main/java/edu/eci/dosw/bowling/BowlingGame.java
+++ bowlin-tdd/src/main/java/edu/eci/dosw/bowling/BowlingGame.java
@@ -105,8 +105,7 @@ public class BowlingGame {
 
     /** true cuando los 10 frames han sido completados. */
     public boolean isComplete() {
-        // TODO: implementar con TDD
-        return false;
+        return currentFrame >= 10;
     }
 
     public List<Frame> getFrames() { return List.copyOf(frames); }
